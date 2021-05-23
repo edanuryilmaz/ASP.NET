9c4bec303254b6129faff8b4b8778b781bd6cbdb
edanuryilmaz <edanuryilmaz30187@gmail.com>
2021-05-23 16:50:28 +0300

    Proje dosyası ekle.

---
+++ ilkwebistem/Controllers/ankara.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ilkwebistem.Controllers
+{
+    public class ankara : Controller
+    {   //[Contoller]/[Aciton]
+        //ankara/nufus
+        public String nufus()
+        {
+            return "Ankaranın nüfusu 6556165";
+        }
+
+        public IActionResult ilceler()
+        {
+            //controller dan view e veri taşıyoruz 
+            ViewBag.toplamilceadedi = 25;
+            return View();
+        }
+    }
+}
